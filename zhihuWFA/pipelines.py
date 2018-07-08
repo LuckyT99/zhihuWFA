3ddf5d19508a64c718f51ef13b13f68525a5fa07
bigyellowdoge <38584846+bigyellowdoge@users.noreply.github.com>
2018-07-08 14:03:37 +0800

    pipelines

--- zhihuWFA/pipelines.py
+++ zhihuWFA/pipelines.py
@@ -6,6 +6,23 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+import jieba
+
 class ZhihuwfaPipeline(object):
+    def createstoplist(stoppath):
+        #print('load stopwords...')
+        stoplist = [line.strip() for line in open(stoppath, 'r', encoding='utf-8').readlines()]
+        stopwords = {}.fromkeys(stoplist)
+        return stopwords
     def process_item(self, item, spider):
+        list=jieba.cut(item['content'],cut_all=False)
+        stop = createstoplist("停用词表.txt")
+        list2=[]
+        for i in list:
+            if i in stop:
+                continue
+            else:
+                list2.append(i)
+        f=open("分词.txt","w",encoding="utf-8")
+        f.write(" ".join(list2))
         return item
